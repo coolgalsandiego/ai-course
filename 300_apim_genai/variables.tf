d90ccd27a2d178f02c698dad6d97f0cd3de24bee
Houssem Dellai <hodellai@microsoft.com>
2024-12-14 00:03:36 +0100

    work in progress

--- 300_apim_genai/variables.tf
+++ 300_apim_genai/variables.tf
@@ -1,7 +1,7 @@
 variable "prefix" {
   description = "Prefix to be used for all resources in this example"
   type        = string
-  default     = "300swc"
+  default     = "300dev"
 }
 
 variable "openAIConfig" {
