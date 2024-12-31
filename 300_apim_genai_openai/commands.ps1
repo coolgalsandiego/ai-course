b049c412687364a803731db5e7e87e2bf3a5f3c7
Houssem Dellai <hodellai@microsoft.com>
2024-12-31 16:47:57 +0100

    work in progress

--- 300_apim_genai_openai/commands.ps1
+++ 300_apim_genai_openai/commands.ps1
@@ -7,10 +7,18 @@ $apimName = "apim-genai-openai-0019"
 az group create --name $RG --location $location
 
 # create API Management with Comsumption mode
-az apim create --name $apimName --resource-group $RG --location $location --sku-name Consumption --publisher-email "noreply@microsoft.com" --publisher-name "Microsoft" --enable-managed-identity
+az apim create --name $apimName --resource-group $RG --location $location `
+        --sku-name Consumption `
+        --publisher-email "noreply@microsoft.com" `
+        --publisher-name "Microsoft" `
+        --enable-managed-identity
 
 # create Azure AI services
-az cognitiveservices account create -n $aiServiceName -g $RG --kind AIServices --sku S0 --location $location --custom-domain $aiServiceName
+az cognitiveservices account create -n $aiServiceName -g $RG `
+   --kind AIServices `
+   --sku S0 `
+   --location $location `
+   --custom-domain $aiServiceName
 
 # create deployment for ChatGPT 4o model
 az cognitiveservices account deployment create -n $aiServiceName -g $RG `
@@ -24,7 +32,10 @@ az cognitiveservices account deployment create -n $aiServiceName -g $RG `
 # create role assignment for API Management to access Azure AI Services
 $apimIdentityPrincipalId = $(az apim show --name $apimName --resource-group $RG --query identity.principalId -o tsv)
 $aiServicesResourceId = $(az cognitiveservices account show --name $aiServiceName --resource-group $RG --query id -o tsv)
-az role assignment create --role "Cognitive Services OpenAI User" --assignee $apimIdentityPrincipalId --scope $aiServicesResourceId
+
+az role assignment create --role "Cognitive Services OpenAI User" `
+        --assignee $apimIdentityPrincipalId `
+        --scope $aiServicesResourceId
 
 # import OpenAPI specification for OpenAI to API Management
 $aiServiceUrl = $(az cognitiveservices account show -n $aiServiceName -g $RG --query properties.endpoint -o tsv)
