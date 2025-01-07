39603184469aaccb8bb89c63f98bc735bd567914
Houssem Dellai <hodellai@microsoft.com>
2025-01-07 06:27:30 +0100

    work in progress

--- 340_apim_genai_semantic_caching/apim-redis.tf
+++ 340_apim_genai_semantic_caching/apim-redis.tf
@@ -1,8 +1,8 @@
 resource "azurerm_api_management_redis_cache" "apim-redis" {
-  name              = "apim-redis"
+  name              = "Default"
   api_management_id = azapi_resource.apim.id
   connection_string = "${azurerm_container_app.aca-redis-stack.name}.${azurerm_container_app_environment.aca-env.default_domain}:6379" # azurerm_redis_cache.redis.primary_connection_string # azurerm_container_app.aca-redis-stack.name # 
-#   description       = "Redis cache instances"
+  description       = "Redis cache instances"
 #   redis_cache_id    = azurerm_redis_cache.redis.id
 #   cache_location    = azurerm_resource_group.rg.location
 }
